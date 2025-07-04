1025a4cad52313567f79091bc0e51748211da4a9
John Ruble <johnruble@gmail.com>
2025-07-04 11:57:48 -0400

    rename custom pio config to avoid warning

--- scripts/git_version.py
+++ scripts/git_version.py
@@ -2,14 +2,14 @@ Import("env", "projenv")
 import subprocess
 
 try:
-    include_hash = env.GetProjectOption("include_git_hash", "false").lower() == "true"
+    include_hash = env.GetProjectOption("custom_include_git_hash", "false").lower() == "true"
     
     if include_hash:
         git_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
         print(f"Using git hash: {git_hash}")
         projenv.Append(CPPDEFINES=[("FW_VERSION_SUFFIX", f'\\"+{git_hash}\\"')])
     else:
-        print("Skipping git hash (include_git_hash = false)")
+        print("Skipping git hash (custom_include_git_hash = false)")
             
 except:
     print("No git repository found, using default empty FW_VERSION_SUFFIX")
