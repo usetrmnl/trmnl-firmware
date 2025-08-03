f5af2722b98b9ff3541126225da1d2ffd70aea27
Larry Bank <bitbank@pobox.com>
2025-08-03 19:24:46 +0100

    Fixed button logic

--- src/button.cpp
+++ src/button.cpp
@@ -5,33 +5,28 @@
 
 ButtonPressResult read_button_presses()
 {
-  bool sampled_high = false;
   auto time_start = millis();
   Log_info("Button time=%d: start", time_start);
-  while (1)
+  ButtonPressResult bpr = NoAction;
+
+  while (digitalRead(PIN_INTERRUPT) == LOW && millis() - time_start < BUTTON_SOFT_RESET_TIME) // while button held
   {
-    auto elapsed = millis() - time_start;
-    auto pin = digitalRead(PIN_INTERRUPT);
-    if(pin == LOW && elapsed > BUTTON_SOFT_RESET_TIME){
-      return SoftReset;
-    }
-    else if (pin == LOW && elapsed > BUTTON_HOLD_TIME)
-    {
-      Log_info("Button time=%d pin=%d: detected long press", elapsed, pin);
-      return LongPress;
-    }
-    else if (pin == LOW && elapsed > BUTTON_MEDIUM_HOLD_TIME) // old double-click
-    {
-      Log_info("Button time=%d pin=%d: detected double-click", elapsed, pin);
-      return DoubleClick;
-    }
-    else if (pin == HIGH) // Releasing the button quickly = no special action
-    {
-      Log_info("Button time=%d pin=%d: detected no-action", elapsed, pin);
-      return NoAction;
-    }
+    delay(10); // can save power if configured correctly
   }
-  return NoAction;
+  auto elapsed = millis() - time_start;
+  if (elapsed >= BUTTON_SOFT_RESET_TIME) {
+      Log_info("Button time=%d detected extra-long press", elapsed);
+      bpr = SoftReset;
+  } else if (elapsed > BUTTON_HOLD_TIME) {
+      Log_info("Button time=%d detected long press", elapsed);
+      bpr = LongPress;
+  } else if (elapsed > BUTTON_MEDIUM_HOLD_TIME) {
+      Log_info("Button time=%d detected double-click", elapsed);
+      bpr = DoubleClick;
+  } else {
+      Log_info("Button time=%d detected no-action", elapsed);
+  }
+  return bpr;
 }
 
 const char *ButtonPressResultNames[] = {
