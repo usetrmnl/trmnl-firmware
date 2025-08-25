7510be6339a7b4782864d571b40012c21e94ffd7
Larry Bank <bitbank@pobox.com>
2025-08-25 21:53:59 +0100

    Changes to bring the Xiao DIY up to the latest firmware

--- src/DEV_Config.h
+++ src/DEV_Config.h
@@ -93,6 +93,13 @@
    #define EPD_RST_PIN  1
    #define EPD_DC_PIN   4
    #define EPD_BUSY_PIN 3
+#elif defined(BOARD_XIAO_EPAPER_DISPLAY)
+   #define EPD_SCK_PIN  7
+   #define EPD_MOSI_PIN 9
+   #define EPD_CS_PIN   44
+   #define EPD_RST_PIN  38
+   #define EPD_DC_PIN   10
+   #define EPD_BUSY_PIN 4
 #else
    #error "Board type not defined. Please define BOARD_WAVESHARE_ESP32_DRIVER or BOARD_TRMNL or BOARD_SEEED_XIAO_ESP32C3 or BOARD_SEEED_XIAO_ESP32S3 in platformio.ini build_flags."
 #endif
