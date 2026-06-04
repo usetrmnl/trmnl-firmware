2c3941adf4189f01f0fe93fbf877db1307602dbe
GoReNeY <goreney.w@gmail.com>
2026-06-04 11:54:11 +0300

    changing MAC address function to not rely on WiFi class

--- src/bl.cpp
+++ src/bl.cpp
@@ -1567,15 +1567,6 @@ void bl_process(void)
 {
 }
 
-static String get_device_mac() {
-  uint8_t mac[6] = {};
-  esp_efuse_mac_get_default(mac);
-  char buf[18];
-  snprintf(buf, sizeof(buf), "%02X:%02X:%02X:%02X:%02X:%02X",
-           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  return String(buf);
-}
-
 ApiDisplayInputs loadApiDisplayInputs(Preferences &preferences)
 {
   ApiDisplayInputs inputs;
