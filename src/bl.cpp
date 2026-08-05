47509c69db1c62379ae5e6dfcd390e0187ae3a15
Larry Bank <bitbank@pobox.com>
2026-08-04 20:37:23 -0700

    Updated screen_wiper to run on X and OG as a plugin name

--- src/bl.cpp
+++ src/bl.cpp
@@ -74,7 +74,7 @@ const char *szHTTPErrors[] = {
 };
 
 static float vBatt;
-
+void display_wipe(void);
 static https_request_err_e downloadAndShow(); // download and show the image
 static https_request_err_e handleApiDisplayResponse(ApiDisplayResponse &apiResponse);
 static void resetDeviceCredentials(void);            // reset device credentials API key, Friendly ID, Wi-Fi SSID and password
@@ -126,7 +126,6 @@ void wait_for_serial() {
 #include "IQS323.h"
 
 void process_iqs323_data(void);
-void display_wipe(void);
 
 #define IQS323_I2C_ADDRESS 0x44
 // Touchbar indicator to redraw after a full-refresh (e.g. logo screen)
@@ -1708,13 +1707,11 @@ static https_request_err_e downloadAndShow()
   }
 
   https_request_err_e result = handleApiDisplayResponse(apiDisplayResult.response);
-#ifdef BOARD_TRMNL_X
-  if (strcmp(apiDisplayResult.response.filename.c_str(), "screen_wiper.png")) {
+  if (strcmp(apiDisplayResult.response.filename.c_str(), "screen_wiper.png") == 0) {
       Log_info("Detected screen wiper filename: %s, starting 20 passes of clearing...\n", apiDisplayResult.response.filename.c_str());
       display_wipe();
       return result; // no image to display
   }
-#endif // BOARD_TRMNL_X
 
   if (!status && result == HTTPS_SUCCESS) { // this means we already have this image stored in SPIFFS
       char szTemp[36];
