f9a78a598b7c9e716e2208d580b8f1fa324b3a19
Larry Bank <bitbank@pobox.com>
2026-09-14 19:57:16 +0200

    Added memory free for completeness

--- src/bl.cpp
+++ src/bl.cpp
@@ -1939,6 +1939,12 @@ https_request_err_e handleApiDisplayResponse(ApiDisplayResponse &apiResponse)
   int file_size = 0;
 
   filesystem_purge_playlist(&apiResponse);
+  if (apiResponse.playlist_count) {
+     // Free the memory since it was allocated with malloc()
+      apiResponse.playlist_count = 0;
+      free(apiResponse.playlist_names);
+      apiResponse.playlist_names = NULL;
+  }
 
 #ifdef BOARD_TRMNL_X
   // Set touchbar mode and persist to NVS
